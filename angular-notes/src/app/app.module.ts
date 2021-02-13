36311e56f0bdd42df35379399dfd64259e2e1247
Oliverjacobb <69274114+Oliverjacobb@users.noreply.github.com>
2021-02-13 13:12:30 +0000

    Notes list component
    Basic set up for the app.component.html toolbar and the notes-list component.

--- angular-notes/src/app/app.module.ts
+++ angular-notes/src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    AddNoteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
